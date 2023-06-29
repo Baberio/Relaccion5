eb045421cd0a21d46411429a3854e31bbc6fb5f6
DonPingo <DonPingo@DESKTOP-EKSUKV2>
2023-06-29 12:03:58 +0200

    Commit 2

--- src/Ejercicios_05/Ejercicio_01.java
+++ src/Ejercicios_05/Ejercicio_01.java
@@ -1,9 +1,31 @@
 package Ejercicios_05;
 
+import java.util.Scanner;
+
+import funciones.FuncionesMatrices;
+
 public class Ejercicio_01 {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
+		// 1. Dado un array de tamaño (N,M) de enteros, inicializarlo a un valor dado A
+		
+		Scanner teclado =new Scanner(System.in);
+		
+		System.out.println("¿Cuantas filas quieres:?");
+		int filas = teclado.nextInt();
+		
+		System.out.println("¿Cuantas columnas quieres:?");
+		int columnas = teclado.nextInt();
+		
+		int matriz [][] = new int [filas][columnas];
+		
+		System.out.println("A que numero quieres inicializar la matirz");
+		int num=teclado.nextInt();
+		
+		System.out.println("Su matriz quedaria:");
+		FuncionesMatrices.inicializar(matriz, num);
+		FuncionesMatrices.mostrarMatriz(matriz);
+		teclado.close();
 
 	}
 
--- src/funciones/FuncionesMatrices.java
+++ src/funciones/FuncionesMatrices.java
@@ -23,4 +23,36 @@ public class FuncionesMatrices {
 		}
 	}
 	
-}
+	public static void inicializar (int m[][], int num) {
+		for (int fila = 0; fila < m.length; fila++) {
+			for (int col = 0; col < m[0].length; col++){
+				m[fila][col]=num;
+			}
+		}
+		System.out.println();
+	}
+	public static boolean sonTodosCero(int m [][] ) {
+	    for (int fila = 0; fila < m.length; fila++) {
+	        for (int col = 0; col < m[fila].length; col++) {
+	            if (m[fila][col] != 0) {
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+	}
+	
+	public static int igualNumero (int m [][], int num) {
+		int contador=0;
+		for (int fila = 0; fila < m.length; fila++) {
+			for(int col=0; col<m[0].length; col++) {
+				if (m [fila][col]== num ){
+					contador = contador+1;
+				}
+				}
+			}
+		return contador;
+		}
+	}
+	
+
